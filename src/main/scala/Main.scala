e79a35dc058b7d7bc04cef904b9e67dc129834f0
Luyang Wang <tmacraftsc2@gmail.com>
2018-12-09 20:53:34 +0800

    reorg project

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,13 @@
 import com.intel.analytics.bigdl.dataset.Sample
-import com.intel.analytics.bigdl.nn.LookupTable
 import com.intel.analytics.bigdl.tensor.{Storage, Tensor}
 import com.intel.analytics.bigdl.utils.T
 import com.intel.analytics.zoo.common.NNContext
-import model.RecRNN
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
-import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.feature._
-import org.apache.spark.ml.linalg.{DenseVector, SparseVector}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
-import collection.JavaConverters._
 import scala.collection.mutable
 
 /**
@@ -30,24 +25,16 @@ object Main extends App{
   val sc = NNContext.initNNContext(conf)
   val spark = SparkSession.builder().config(conf).getOrCreate()
 
-
   val data = spark.read.options(Map("header" -> "true", "delimiter" -> "|")).csv("./modelFiles/recRNNsample.csv")
-//      .groupBy("SESSION_ID").agg(collect_list("SKU_NUM").alias("SKU"))
-//  data.show()
-
-//  val data1 = data.groupBy("SESSION_ID").agg(collect_list("SKU_NUM").alias("SKU"))
 
   val skuCount = data.select("SKU_NUM").distinct().count().toInt
   val skuIndexer = new StringIndexer().setInputCol("SKU_NUM").setOutputCol("SKU_INDEX").setHandleInvalid("keep")
   val labelIndexer = new StringIndexer().setInputCol("out").setOutputCol("label").setHandleInvalid("keep")
-  val oheIndexer = new OneHotEncoder().setInputCol("SKU_INDEX").setOutputCol("item")
-  val pipeline = new Pipeline().setStages(Array(skuIndexer, oheIndexer))
 
   val skuIndexerModel = skuIndexer.fit(data)
 
   val data1 = skuIndexerModel.transform(data)
     .withColumn("SKU_INDEX", col("SKU_INDEX")+1)
-
 
   data1.show()
   data1.printSchema()
@@ -57,8 +44,6 @@ object Main extends App{
 
   data2.show()
   data2.printSchema()
-
-//  val pipelineModel = pipeline.fit(data)
 
   val skuPadding = Array.fill[Double](10)(0.0)
 
@@ -82,44 +67,8 @@ object Main extends App{
   })
 
   val outSize = data4.map(_._2).distinct().count().toInt
-
   println(outSize)
 
-////  val skuPadding1 = Array.fill[Array[Double]](5)(skuPadding)
-////
-////  val word2Vec = new Word2Vec().setInputCol("SKU").setOutputCol("Vec").setVectorSize(200).setMinCount(0)
-////  val vecModel = word2Vec.fit(data1)
-////  vecModel.write.overwrite().save("./modelFiles/skuEmbedding")
-////  val result = vecModel.transform(data1)
-////  result.show()
-////
-////  val lookUp = vecModel.getVectors.withColumnRenamed("word", "SKU_NUM")
-////  lookUp.show()
-////  lookUp.printSchema()
-////
-////  val data2 = data.join(lookUp, Seq("SKU_NUM"))
-////  data2.show()
-////
-////  import spark.implicits._
-////  val data3 = data2
-////    .groupBy("SESSION_ID").agg(collect_list("vector").alias("item"), collect_list("SKU_NUM").alias("sku"))
-////      .rdd.map(r => {
-//////    val session = r.getAs[String]("SESSION_ID")
-////    val item = r.getAs[mutable.WrappedArray[DenseVector]]("item").array.map(x=>x.toArray)
-////    val item1 = skuPadding1 ++ item
-////    val item2 = item1.takeRight(6)
-////    val label = r.getAs[mutable.WrappedArray[String]]("sku").array.takeRight(1).head
-////    val features = item2.dropRight(1)
-////    (features, label)
-////  })
-////    .toDF("SKU", "out")
-////
-////  val labelIndexerModel = labelIndexer.fit(data3)
-////  val data4 = labelIndexerModel.transform(data3)
-////    .withColumn("label", col("label") + 1)
-////
-////  val outSize = data4.select("label").distinct().count().toInt
-////
   val trainSample = data4.map(r => {
     val label = Tensor[Double](T(r._2))
     val array = r._1
@@ -130,12 +79,8 @@ object Main extends App{
   println("Sample feature print: "+ trainSample.take(1).head.feature())
   println("Sample label print: " + trainSample.take(1).head.label())
 
-//  val layer = LookupTable[Double](skuCount, 200)
-//  val input = Tensor(Storage(Array(5.0f, 2.0f, 6.0f, 9.0f, 4.0f)), 1, Array(5))
-
-
   val rnn = new RecRNN()
   val model = rnn.buildModel(outSize, skuCount)
-  rnn.train(model, trainSample, "./modelFiles/rnnModel", 4)
+  rnn.train(model, trainSample, "./modelFiles/rnnModel", 4, 3)
 
 }
