bb66eedcb92fc2747104bd1cc607a5502a93c188
Luyang Wang <tmacraftsc2@gmail.com>
2018-12-11 21:40:57 +0800

    add embedding layers

--- src/main/scala/RecRNN.scala
+++ src/main/scala/RecRNN.scala
@@ -1,9 +1,10 @@
 import com.intel.analytics.bigdl.Module
 import com.intel.analytics.bigdl.dataset.Sample
-import com.intel.analytics.bigdl.nn.ClassNLLCriterion
+import com.intel.analytics.bigdl.nn.{Sequential => _, _}
 import com.intel.analytics.bigdl.nn.abstractnn.AbstractModule
 import com.intel.analytics.bigdl.visualization.{TrainSummary, ValidationSummary}
 //import com.intel.analytics.bigdl.nn.keras._
+import com.intel.analytics.bigdl.utils.Shape
 import com.intel.analytics.bigdl.nn._
 import com.intel.analytics.bigdl.optim._
 import com.intel.analytics.bigdl.tensor.Tensor
@@ -15,26 +16,31 @@ import org.apache.spark.rdd.RDD
   */
 class RecRNN {
 
-  def buildModel(numClasses: Int, skuCount: Int): Sequential[Double] = {
+  def buildModel(numClasses: Int, skuCount: Int, maxLength: Int): Sequential[Double] = {
     val model = Sequential[Double]()
+    val branches = Concat[Double](2)
+
+    var embedWidth = 0
+    (1 to maxLength).foreach { _ =>
+      val lookupTable = LookupTable[Double](skuCount, 50)
+      lookupTable.setWeightsBias(
+        Array(Tensor[Double](skuCount, 50).randn(0, 0.1)))
+      branches.add(
+        Sequential[Double]()
+          .add(Select[Double](2, 1 + embedWidth))
+          .add(AddConstant(1.0))
+          .add(lookupTable))
+          .setName(s"branch${embedWidth + 1}")
+      embedWidth += 1
+    }
 
     model
-      .add(AddConstant(1))
-      .add(LookupTable[Double](skuCount, 300))
-      .add(TemporalConvolution(300, 250, 3))
-      .add(ReLU())
-      .add(TemporalMaxPooling(3))
+      .add(branches)
+      .add(BiRecurrent[Double](JoinTable[Double](2, 2).asInstanceOf[AbstractModule[Table, Tensor[Double], Double]]).add(GRU(50, 200)))
       .add(Dropout(0.2))
-      .add(BiRecurrent[Double](JoinTable[Double](2, 2).asInstanceOf[AbstractModule[Table, Tensor[Double], Double]]).add(GRU(250, 200)))
       .add(Select(2, -1))
       .add(Linear[Double](400, numClasses))
       .add(LogSoftMax())
-
-//    model
-////        .add(Flatten(Shape(Array(1, 5, 98))))
-//        .add(Embedding(skuCount, 200, inputShape = Shape(10)))
-//        .add(GRU(200))
-//        .add(Dense(numClasses, activation = "sigmoid"))
 
     model
   }
@@ -47,39 +53,25 @@ class RecRNN {
              batchSize: Int
            ):Module[Double] = {
 
-    val rmsp = new RMSprop[Double]()
-    val adagrad = new Adagrad[Double]()
-//    val loss = new ClassNLLCriterion[Double]()
-    val loss = new CrossEntropyCriterion[Double]()
-
-
-//      model.compile(
-//        rmsp,
-//        loss
-//  //      Array(new Loss[Float]().asInstanceOf[ValidationMethod[Float]])
-//      )
-//      model.fit(train, batchSize)
-
     val split = train.randomSplit(Array(0.8, 0.2), 100)
     val trainRDD = split(0)
     val testRDD = split(1)
     val optimizer = Optimizer(
       model = model,
       sampleRDD = trainRDD,
-      criterion = loss,
+      criterion = new CrossEntropyCriterion[Double](),
       batchSize = batchSize
     )
 
     val trained_model = optimizer
-      .setOptimMethod(rmsp)
+      .setOptimMethod(new RMSprop[Double]())
 //      .setTrainSummary(new TrainSummary("./modelFiles", "recRNNTrainingSum"))
 //      .setValidationSummary(new ValidationSummary("./modelFiles", "recRNNValidationSum"))
-      .setValidation(Trigger.everyEpoch, testRDD, Array(new Top5Accuracy[Double]()), batchSize)
+      .setValidation(Trigger.everyEpoch, testRDD, Array(new Top1Accuracy[Double]()), batchSize)
       .setEndWhen(Trigger.maxEpoch(maxEpoch))
       .optimize()
 
     trained_model.saveModule(modelPath, null, overWrite = true)
-
     println("model has been saved")
 
     trained_model
