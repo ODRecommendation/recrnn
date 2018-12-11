f878c97737523e5e0b4540e0411e5cbdfc585a1e
Luyang Wang <tmacraftsc2@gmail.com>
2018-12-11 11:19:18 +0800

    in progress

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,7 +4,9 @@ import com.intel.analytics.bigdl.utils.T
 import com.intel.analytics.zoo.common.NNContext
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
+import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.feature._
+import org.apache.spark.ml.linalg.SparseVector
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
@@ -30,58 +32,95 @@ object Main extends App{
   val skuCount = data.select("SKU_NUM").distinct().count().toInt
   val skuIndexer = new StringIndexer().setInputCol("SKU_NUM").setOutputCol("SKU_INDEX").setHandleInvalid("keep")
   val labelIndexer = new StringIndexer().setInputCol("out").setOutputCol("label").setHandleInvalid("keep")
+  val ohe = new OneHotEncoder().setInputCol("SKU_INDEX").setOutputCol("vectors")
 
-  val skuIndexerModel = skuIndexer.fit(data)
+  val pipeline = new Pipeline().setStages(Array(skuIndexer, ohe))
+  val pipelineModel = pipeline.fit(data)
 
-  val data1 = skuIndexerModel.transform(data)
-    .withColumn("SKU_INDEX", col("SKU_INDEX")+1)
+  val data1 = pipelineModel.transform(data)
 
   data1.show()
   data1.printSchema()
 
+  val featureRow = data1.select("vectors").head
+  val inputLayer = featureRow(0).asInstanceOf[SparseVector].size
+
+  println(inputLayer)
+
   val data2 = data1.groupBy("SESSION_ID")
-    .agg(collect_list("SKU_INDEX").alias("item"), collect_list("SKU_NUM").alias("SKU"))
+    .agg(collect_list("vectors").alias("item"))
 
-  data2.show()
   data2.printSchema()
+  data2.show()
 
-  val skuPadding = Array.fill[Double](10)(0.0)
-  val bcPadding = sc.broadcast(skuPadding).value
+  val skuPadding = Array.fill[Double](inputLayer)(0.0)
+  val skuPadding1 = Array.fill[Array[Double]](10)(skuPadding)
+  val bcPadding = sc.broadcast(skuPadding1).value
 
   import spark.implicits._
   val data3 = data2.rdd.map(r => {
-    val item = r.getAs[mutable.WrappedArray[java.lang.Double]]("item").array.map(_.doubleValue())
-    val labelRaw = r.getAs[mutable.WrappedArray[String]]("SKU").array
+    val item = r.getAs[mutable.WrappedArray[SparseVector]]("item").array
     val item1 = bcPadding ++ item
     val item2 = item1.takeRight(11)
-    val label = labelRaw.takeRight(1).head
+    val label = item1.takeRight(1).head
     val features = item2.dropRight(1)
     (features, label)
   }).toDF("features", "out")
 
-  val labelIndexerModel = labelIndexer.fit(data3)
+  data3.printSchema()
+  data3.show()
 
-  val data4 = labelIndexerModel.transform(data3).rdd.map(r => {
-    val features = r.getAs[mutable.WrappedArray[java.lang.Double]]("features").array.map(_.doubleValue())
-    val label = r.getAs[Double]("label") + 1.0
-    (features, label)
-  })
-
-  val outSize = data4.map(_._2).distinct().count().toInt
-  println(outSize)
-
-  val trainSample = data4.map(r => {
-    val label = Tensor[Double](T(r._2))
-    val array = r._1
-    val vec = Tensor(Storage(array), 1, Array(10))
-    Sample(vec, label)
-  })
-
-  println("Sample feature print: "+ trainSample.take(1).head.feature())
-  println("Sample label print: " + trainSample.take(1).head.label())
-
-  val rnn = new RecRNN()
-  val model = rnn.buildModel(outSize, skuCount)
-  rnn.train(model, trainSample, "./modelFiles/rnnModel", 100, 8)
+//  val skuIndexerModel = skuIndexer.fit(data)
+//
+//  val data1 = skuIndexerModel.transform(data)
+//    .withColumn("SKU_INDEX", col("SKU_INDEX")+1)
+//
+//  data1.show()
+//  data1.printSchema()
+//
+//  val data2 = data1.groupBy("SESSION_ID")
+//    .agg(collect_list("SKU_INDEX").alias("item"), collect_list("SKU_NUM").alias("SKU"))
+//
+//  data2.show()
+//  data2.printSchema()
+//
+//  val skuPadding = Array.fill[Double](10)(0.0)
+//  val bcPadding = sc.broadcast(skuPadding).value
+//
+//  import spark.implicits._
+//  val data3 = data2.rdd.map(r => {
+//    val item = r.getAs[mutable.WrappedArray[java.lang.Double]]("item").array.map(_.doubleValue())
+//    val labelRaw = r.getAs[mutable.WrappedArray[String]]("SKU").array
+//    val item1 = bcPadding ++ item
+//    val item2 = item1.takeRight(11)
+//    val label = labelRaw.takeRight(1).head
+//    val features = item2.dropRight(1)
+//    (features, label)
+//  }).toDF("features", "out")
+//
+//  val labelIndexerModel = labelIndexer.fit(data3)
+//
+//  val data4 = labelIndexerModel.transform(data3).rdd.map(r => {
+//    val features = r.getAs[mutable.WrappedArray[java.lang.Double]]("features").array.map(_.doubleValue())
+//    val label = r.getAs[Double]("label") + 1.0
+//    (features, label)
+//  })
+//
+//  val outSize = data4.map(_._2).distinct().count().toInt
+//  println(outSize)
+//
+//  val trainSample = data4.map(r => {
+//    val label = Tensor[Double](T(r._2))
+//    val array = r._1
+//    val vec = Tensor(Storage(array), 1, Array(10))
+//    Sample(vec, label)
+//  })
+//
+//  println("Sample feature print: "+ trainSample.take(1).head.feature())
+//  println("Sample label print: " + trainSample.take(1).head.label())
+//
+//  val rnn = new RecRNN()
+//  val model = rnn.buildModel(outSize, skuCount)
+//  rnn.train(model, trainSample, "./modelFiles/rnnModel", 100, 8)
 
 }
